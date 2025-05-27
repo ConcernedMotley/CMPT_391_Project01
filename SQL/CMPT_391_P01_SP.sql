a7fb786084e911b76d5ab90b438854cab937e774
Nati Solomon <71053656+Nati101@users.noreply.github.com>
2025-05-27 13:56:06 -0600

    Update CMPT_391_P01_SP.sql
    SP for Search

--- SQL/CMPT_391_P01_SP.sql
+++ SQL/CMPT_391_P01_SP.sql
@@ -10,3 +10,21 @@ USE CMPT_391_P01;
 --Select * from StudentCredentials;
 --select * from Takes;
 --select * from Cart;
+
+USE CMPT_391_P01;
+GO
+
+-- Drop if it already exists
+IF OBJECT_ID('sp_GetCoursesByLabel', 'P') IS NOT NULL
+    DROP PROCEDURE sp_GetCoursesByLabel;
+GO
+
+CREATE PROCEDURE sp_GetCoursesByLabel
+    @Label VARCHAR(10)
+AS
+BEGIN
+    SELECT CourseID, CourseLabel, courseName, credits, prereq
+    FROM Course
+    WHERE CourseLabel LIKE @Label + '%'
+END;
+GO
